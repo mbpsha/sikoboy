2e348bef03c8bacfaf4397a063255f85d7274306
ananditaartiar <ananditaputriartiar@student.uns.ac.id>
2026-05-11 10:27:35 +0700

    ajuan kerjasama

--- routes/web.php
+++ routes/web.php
@@ -324,7 +324,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
     Route::post('/data-kerjasama/{id}/proses',
         [DataKerjasamaController::class, 'storeProcess'])
         ->name('data-kerjasama.proses.store');
-    Route::post('/data-kerjasama/{id}/proses/{prosesId}',
+    Route::put('/data-kerjasama/{id}/proses/{prosesId}',
         [DataKerjasamaController::class, 'updateProcess'])
         ->name('data-kerjasama.proses.update');
 
